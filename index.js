24609a8496a12e2125c7ff28323a1515b8389125
purajit <7026198+purajit@users.noreply.github.com>
2025-10-23 01:04:36 -0700

    add artifactory, apt-cacher-ng, UI improvements, config simplify type vs name (#1)

--- index.js
+++ index.js
@@ -28,14 +28,18 @@ function itemRow(it, dc_idx, it_idx) {
     iconurl = "./images/ceph.png";
   } else if (it.type === "drp") {
     iconurl = "./images/drp.png";
+  } else if (it.type === "artifactory") {
+    iconurl = "./images/artifactory.png";
+  } else if (it.type === "apt-cacher-ng") {
+    iconurl = "./images/apt-cacher-ng.png";
   }
   return `
-  <div class="item-container flex-row" onclick="selectItem(${dc_idx}, ${it_idx})">
+  <div class="item-container flex-row" id="item-${dc_idx}-${it_idx}" onclick="selectItem(${dc_idx}, ${it_idx})">
     <div class="icon">
       <img width=20px src="${iconurl}">
     </div>
     <div class="item flex-col">
-        <div class="name">${it.name}</div>
+        <div class="name">${it.type || it.name}</div>
         <div class="meta">${meta}</div>
     </div>
   </div>`;
@@ -43,6 +47,10 @@ function itemRow(it, dc_idx, it_idx) {
 
 async function selectItem(dc_idx, it_idx) {
   const it = (await config).datacenters[dc_idx].items[it_idx];
+  [...document.getElementsByClassName("item-container")].forEach((el) =>
+    el.classList.remove("selected"),
+  );
+  document.getElementById(`item-${dc_idx}-${it_idx}`).classList.add("selected");
   openUrl(it.url);
 }
 
